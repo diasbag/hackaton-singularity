5f78d3c7e00072cb0446dcb8404294fa15e3ef5c
dias.bagzat <diasbagzat@gmail.com>
2022-06-22 12:48:19 +0600

    Commit

---
+++ src/main/java/com/example/turnir/TurnirApplication.java
@@ -0,0 +1,13 @@
+package com.example.turnir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turnir/TurnirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turnir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
