61abbf8146fc63a9d18400760eba5d8b5bb8cc69
dias.bagzat <diasbagzat@gmail.com>
2022-06-22 12:52:13 +0600

    Commit

--- src/main/java/com/example/turnir/TurnirApplication.java
+++ src/main/java/com/example/turnir/TurnirApplication.java
@@ -3,6 +3,7 @@ package com.example.turnir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Token: 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
 @SpringBootApplication
 public class TurnirApplication {
 
